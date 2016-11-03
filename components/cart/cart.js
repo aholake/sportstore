603c5ec7cdb1f87aa0ab1a883773cd76a43bb8d8
aholake <nluit.tanloc@gmail.com>
2016-11-03 08:20:48 +0700

    Add cart summary directive

--- components/cart/cart.js
+++ components/cart/cart.js
@@ -1,7 +1,61 @@
 angular.module('cart', [])
-	.factory('cart', ['', function(){
-		var cartData = [];
-		return {
-			
-		}
-	}])
+    .factory('cart', function() {
+        var cartData = [];
+        return {
+            addProduct: function(id, name, price) {
+                var addedToExistingItem = false;
+                for (var i = cartData.length - 1; i >= 0; i--) {
+                    if (cartData[i].id == id) {
+                        cartData[i].count++;
+                        addedToExistingItem = true;
+                        break;
+                    }
+                }
+
+                if (!addedToExistingItem) {
+                    cartData.push({
+                        count: 1,
+                        id: id,
+                        name: name,
+                        price: price
+                    });
+                }
+            },
+            removeProduct: function(id) {
+                for (var i = cartData.length - 1; i >= 0; i--) {
+                    if (cartData[i].id == id) {
+                        cartData.splice(i, 1);
+                        break;
+                    }
+                }
+            },
+
+            getProducts: function() {
+                return cartData;
+            }
+        }
+    })
+    .directive("cartSummary", function(cart) {
+        return {
+            restrict: "E",
+            templateUrl: '../components/cart/cartSummary.html',
+            controller: function($scope) {
+                var cartData = cart.getProducts;
+                $scope.total = function() {
+                    var total = 0;
+                    angular.forEach(cartData, function(key, value) {
+                        total += value.price;
+                    });
+                    return total;
+                }
+
+                $scope.itemCount = function() {
+                    var count = 0;
+                    angular.forEach(cartData, function(key, value) {
+                        count += value.count;
+                    });
+                    return count;
+                }
+            }
+        }
+    })
